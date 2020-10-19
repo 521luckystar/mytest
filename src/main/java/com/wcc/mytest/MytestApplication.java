85e57d1b8a7d98f82f6e58147cefb32b6a9efe2c
admin <2415924884@qq.com>
2020-10-19 17:53:34 +0800

    初始化代码

---
+++ src/main/java/com/wcc/mytest/MytestApplication.java
@@ -0,0 +1,14 @@
+package com.wcc.mytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class MytestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MytestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/wcc/mytest/controller/test.java
@@ -0,0 +1,13 @@
+package com.wcc.mytest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class test {
+    @RequestMapping("/hello")
+    public String hello(){
+        return " hello springboot !";
+    }
+
+}

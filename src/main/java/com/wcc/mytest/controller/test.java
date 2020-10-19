52e5fb52dec8c47ccd382746f3c7b7bbccb2c109
admin <2415924884@qq.com>
2020-10-19 18:05:19 +0800

    测试push

--- src/main/java/com/wcc/mytest/controller/test.java
+++ src/main/java/com/wcc/mytest/controller/test.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class test {
     @RequestMapping("/hello")
     public String hello(){
-        return " hello springboot !";
+        return "wangchenchen, hello springboot !";
     }
 
 }
